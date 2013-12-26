bfbdf97fce480b97043504f0f6cccd919ebc8f51
Todor B. Mihaylov <tbmihailov@gmail.com>
2013-12-26 04:36:33 +0200

    Updated BFS tests - Added Assertions for node process log

--- AI/AI.Graphs.Tests/SearchHelpers_BFS_Tests.cs
+++ AI/AI.Graphs.Tests/SearchHelpers_BFS_Tests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 
 namespace AI.Graphs.Tests
 {
@@ -7,12 +8,24 @@ namespace AI.Graphs.Tests
     public class SearchHelpers_BFS_Tests
     {
         /// <summary>
+        /// Checks if two lists of typ List<int> are equal. 
+        /// Comparing is by element
+        /// </summary>
+        /// <param name="list1"></param>
+        /// <param name="list2"></param>
+        /// <returns></returns>
+        public bool AreEqual(List<int> list1, List<int> list2)
+        {
+            return ListHelpers<int>.AreEqual(list1, list2);
+        }
+
+        /// <summary>
         /// New graph root int node from wikipedia sample http://en.wikipedia.org/wiki/Breadth-first_search
         /// </summary>
         /// <returns>Graph root</returns>
         public Node<int> Get_InitNode_With_Wikipedia_Sample()
         {
-           
+
             var graphRoot = N(1,
                                 N(2,
                                     N(5,
@@ -33,36 +46,45 @@ namespace AI.Graphs.Tests
         public void Test_If_FindValueUsingBFS_Works_Properly_With_FirstNode()
         {
             var graphRoot = Get_InitNode_With_Wikipedia_Sample();
+            var log = new List<int>();
+            var foundNode = SearchHelpers<int>.FindValueUsingBFS(graphRoot, (val => val == 1), log);
 
-            var foundNode = SearchHelpers<int>.FindValueUsingBFS(graphRoot, (val => val == 1));
             Assert.AreEqual(foundNode.Value, 1);
+            AreEqual(log, new List<int>() { 1 });
         }
+
 
         [TestMethod]
         public void Test_If_FindValueUsingBFS_Works_Properly_With_Node_Without_Children()
         {
             var graphRoot = Get_InitNode_With_Wikipedia_Sample();
+            var log = new List<int>();
+            var foundNode = SearchHelpers<int>.FindValueUsingBFS(graphRoot, (val => val == 10), log);
 
-            var foundNode = SearchHelpers<int>.FindValueUsingBFS(graphRoot, (val => val == 10));
             Assert.AreEqual(foundNode.Value, 10);
+            AreEqual(log, new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
         }
 
         [TestMethod]
         public void Test_If_FindValueUsingBFS_Works_Properly_With_Node_With_Children()
         {
             var graphRoot = Get_InitNode_With_Wikipedia_Sample();
+            var log = new List<int>();
+            var foundNode = SearchHelpers<int>.FindValueUsingBFS(graphRoot, (val => val == 5), log);
 
-            var foundNode = SearchHelpers<int>.FindValueUsingBFS(graphRoot, (val => val == 5));
             Assert.AreEqual(foundNode.Value, 5);
+            AreEqual(log, new List<int>() { 1, 2, 3, 4, 5 });
         }
 
         [TestMethod]
         public void Test_If_FindValueUsingBFS_Works_Properly_Without_Match()
         {
             var graphRoot = Get_InitNode_With_Wikipedia_Sample();
+            var log = new List<int>();
+            var foundNode = SearchHelpers<int>.FindValueUsingBFS(graphRoot, (val => val == 66), log);
 
-            var foundNode = SearchHelpers<int>.FindValueUsingBFS(graphRoot, (val => val == 66));
             Assert.IsNull(foundNode);
+            AreEqual(log, new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 });
         }
 
         public Node<int> N(int value, params Node<int>[] children)
